cc9e9eb84c8e953766d7dfafed913cf739b15ed2
Samuel Pimentel <s4muelpimentel@gmail.com>
2022-03-30 23:41:38 -0300

    test: should add user with complete data to mailing list

---
+++ test/usecases/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,21 @@
+import { UserData } from "../../src/usecases/user-data";
+
+describe("Register user on mailing list use case", () => {
+  test("should add user with complete data to mailing list", async () => {
+    const users: UserData[] = [];
+    console.log(users);
+    // const repository: UserRepository = new InMemoryUserRepository(users);
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
+    //   repository
+    // );
+
+    // const name = "any_name";
+    // const email = "any@email.com";
+
+    // await useCase.registerUserOnMailingList({ name, email });
+
+    // const user = repository.findUserByEmail("any@email.com");
+
+    // expect((await user).name).toBe("any_name");
+  });
+});
